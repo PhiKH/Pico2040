a0cdd2e745c0ef91f1d021358e736840c38125ef
PhiKH <pichakhchi.s@yandex.ru>
2023-08-05 21:21:03 +0300

    Oscilloscope v0.1

--- Oscilloscope/OSC4.py
+++ Oscilloscope/OSC4.py
@@ -11,8 +11,8 @@ from matplotlib.backends.backend_qt5agg import FigureCanvas
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 
 flag1 = 0
-limX = 100
-period = 10
+limX = 500
+period = 1
 
 class MyWindow(QtWidgets.QMainWindow):
     def __init__(self):
@@ -23,11 +23,22 @@ class MyWindow(QtWidgets.QMainWindow):
 
         self.n_data = limX
         self.xdata = list(range(self.n_data))
-        self.ydata = [random.randint(0, 10) for i in range(self.n_data)]
-        self.zdata = [random.randint(0, 10) for i in range(self.n_data)]
-        self._line1, = self.ax1.plot(self.xdata, self.ydata, "r")
-        self._line2, = self.ax1.plot(self.xdata, self.zdata, "g")
+        self.adata = [random.randint(0, 10) for i in range(self.n_data)]
+        self._line1, = self.ax1.plot(self.xdata, self.adata, "r")
 
+        self.bdata = [random.randint(10, 20) for i in range(self.n_data)]
+        self._line2, = self.ax1.plot(self.xdata, self.bdata, "g")
+
+        self.cdata = [random.randint(20, 30) for i in range(self.n_data)]
+        self._line3, = self.ax1.plot(self.xdata, self.cdata, "b")
+
+        self.ddata = [random.randint(30, 40) for i in range(self.n_data)]
+        self._line4, = self.ax1.plot(self.xdata, self.ddata, "y")
+
+        # self.ax1.title('OSC')
+        # self.ax1.xlabel('Частота, КГц')
+        # self.ax1.ylabel('Амплитуда')
+        self.ax1.grid(1, 'both', 'both')
         self.plotWidget = FigureCanvas(self.fig)
         lay = QtWidgets.QVBoxLayout(self.content_plot)
         lay.setContentsMargins(0, 0, 0, 0)
@@ -41,12 +52,21 @@ class MyWindow(QtWidgets.QMainWindow):
 
     def _update_canvas(self):
         if flag1:
-            self.ydata = self.ydata[1:] + [random.randint(0, 10)]
-            self.zdata = self.zdata[1:] + [random.randint(0, 10)]
-            self._line1.set_data(self.xdata, self.ydata)
+            self.adata = self.adata[1:] + [random.randint(0, 10)]
+            self._line1.set_data(self.xdata, self.adata)
             self._line1.figure.canvas.draw()
-            self._line2.set_data(self.xdata, self.zdata)
+
+            self.bdata = self.bdata[1:] + [random.randint(10, 20)]
+            self._line2.set_data(self.xdata, self.bdata)
             self._line2.figure.canvas.draw()
+
+            self.cdata = self.cdata[1:] + [random.randint(20, 30)]
+            self._line3.set_data(self.xdata, self.cdata)
+            self._line3.figure.canvas.draw()
+
+            self.ddata = self.ddata[1:] + [random.randint(30, 40)]
+            self._line4.set_data(self.xdata, self.ddata)
+            self._line4.figure.canvas.draw()
             # print('UP')
 
 if __name__ == '__main__':
@@ -62,7 +82,7 @@ if __name__ == '__main__':
             flag1 = 1
 
     def updateSLD1():
-        window.LCD1.display(window.SLD1.value())
+        # window.LCD1.display(window.SLD1.value())
         global limX
         limX = window.SLD1.value()
 
